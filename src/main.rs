64f16e91cbc02e0eda617761ac2097eb5e6a3217
csk <karthik-subramanyam.chakka@grenoble-inp.org>
2021-02-07 20:31:16 +0100

    convert to vector

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,20 @@ const N:usize = 4;
 const MIN:i32 = -100;
 const MAX:i32 = 100;
 
-fn print2d(a: [[i32;N]; M]){
+use rand::Rng;
+
+fn print2d(v: &Vec<Vec<i32>>){
     print!("[");
     for i in 0..M{
-        print!("{:?}", a[i]);
+        print!("{:?}", v[i]);
         if i != M-1{
             println!(",");
         }
     }
     println!("]");
-
 }
 
-use rand::Rng;
-
-fn gen_2d_arr_rand(mut a: [[i32;N]; M]) -> [[i32;N]; M] {
+fn gen_2d_arr_rand(mut v: Vec<Vec<i32>>) -> Vec<Vec<i32>>{
     let mut temp: [i32; M] = [0; M];
     for i in 0..M{
         temp[i] = rand::thread_rng().gen_range(MIN..MAX);
@@ -26,42 +25,61 @@ fn gen_2d_arr_rand(mut a: [[i32;N]; M]) -> [[i32;N]; M] {
     temp.sort();
     temp.reverse();
     for i in 0..M{
-        a[i][0] = temp[i];
+        v[i][0] = temp[i];
     }
     for j in 1..N{
-        if a[0][j-1] == MIN{
-            a[0][j] = MIN;
+        if v[0][j-1] == MIN{
+            v[0][j] = MIN;
         }  else {
-            a[0][j] = rand::thread_rng().gen_range(MIN..a[0][j-1]);
+            v[0][j] = rand::thread_rng().gen_range(MIN..v[0][j-1]);
         }
     }
     for i in 1..M{
         for j in 1..N{
             let less;
-            if a[i][j-1] <= a[i-1][j]{
-                less = a[i][j-1];
+            if v[i][j-1] <= v[i-1][j]{
+                less = v[i][j-1];
             } else {
-                less = a[i-1][j]
+                less = v[i-1][j]
             }
             if less == MIN{
-                a[i][j] = less;
+                v[i][j] = less;
             } else {
-                a[i][j] = rand::thread_rng().gen_range(MIN..less);
+                v[i][j] = rand::thread_rng().gen_range(MIN..less);
             }
         }
     }
-    return a;
+    return v;
 }
 
+fn naive_version(v: &Vec<Vec<i32>>) -> i32{
+    let mut cnt = 0;
+    for i in 0..M{
+        for j in 0..N{
+            if v[i][j] < 0 {
+                cnt = cnt + 1;
+            }
+        }
+    }
+    return cnt;
+}
 
-fn main() {
-    let mut a: [[i32;N]; M] = [[0; N]; M];
-    a = gen_2d_arr_rand(a);
-    print2d(a);
-    // println!("0th row: {:?}", a[0]);
-    // println!("0th ele: {:?}", a[0][0]);
-    // println!("M: {}", a.len());
-    // println!("N: {}", a[0].len());
-    // println!("0th ele: {}", a[0].first().unwrap());
-    // println!("last ele: {}", a[M-1].last().unwrap());
+fn recursive_version(v_slice: &[Vec<i32>]){
+    let n = v_slice.len();
+    if n == 1 {
+        println!("{:?}", v_slice);
+    } else {
+        let mid = n / 2;
+        recursive_version(&v_slice[0..mid]);
+        recursive_version(&v_slice[mid..n]);
+    }
+}
+
+fn main(){
+    let mut v = vec![vec![0;N];M];
+    v = gen_2d_arr_rand(v);
+    print2d(&v);
+    let c = naive_version(&v);
+    println!("c {}", c);
+    recursive_version(&v);
 }
